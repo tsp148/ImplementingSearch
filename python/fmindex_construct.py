2a323954be51324a2b49730d21cd897ded7be92f
Tim Paetsch <paet02@fu-berlin.de>
2026-01-26 01:11:41 +0100

    variable index naming.

--- python/fmindex_construct.py
+++ python/fmindex_construct.py
@@ -4,6 +4,7 @@ import time
 
 parser = argparse.ArgumentParser(description="Naive search for pattern occurrences in reference sequences.")
 parser.add_argument('--reference', type=str, required=True, help='Reference file (FASTA)')
+parser.add_argument('--index', type=str, required=True, help='Index file (created with fmindex_construct.py)')
 
 args = parser.parse_args()
 
@@ -12,4 +13,4 @@ for record in iv2py.fasta.reader(file=args.reference): #"../data/hg38_partial.fa
     reference.append(str(record.seq))
 
 index = iv2py.fmindex(reference=reference)
-index.save("py_index.idx")
+index.save(args.index)
